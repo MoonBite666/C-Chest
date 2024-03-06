107ee61cd213dbbbed723955898a8a08ca09afe2
MoonFeather <moonbite233@gmail.com>
2024-03-06 22:50:29 +0800

    add basic function.

--- game.h
+++ game.h
@@ -1,12 +1,16 @@
 
 #include <stdbool.h>
-
+#include "save.h"
 void Generate_map(int *crt_map, int stage);
 void Load(int stage);
 bool Select(int *sel, int row);
 bool Next_frame(int *crt_map);
+
+void Move_player(int *crt_map, int player_pos, int target_pos);
+void Move_chest(int *crt_map, int chest_pos, int target_pos);
 void Move(int target, int *crt_map, int dir);
 bool Movable(int target, int target_behind);
 bool Map_cycle(int *crt_map);
 
 bool Checkwin(int *map);
+void Undo_command(int *crt_map, Stack *s);
--- save.c
+++ save.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "save.h"
+
 
 int crt_stage;
 int farthest;
 
-void Save_map(int *crt_map){
+void Save_map(const int *crt_map){
     
     FILE *file;
     file = fopen("save.bin", "wb");
@@ -32,4 +34,43 @@ bool Read_map(int *crt_map){
     fclose(file);
 
     return true;
+}
+
+
+void Stack_init(Stack* s) {
+    s->arr = (Command*)malloc(MAX_UNDO * sizeof(Command));
+    s->top = -1;
+}
+
+int Stack_isEmpty(const Stack* s) {
+    return s->top == -1;
+}
+
+int Stack_isFull(const Stack* s) {
+    return s->top == MAX_UNDO - 1;
+}
+
+void Stack_push(Stack* s, Command value) {
+    if (Stack_isFull(s)) {
+        printf("Stack is full!\n");
+        return;
+    }
+    s->arr[++(s->top)] = value;
+}
+
+Command Stack_pop(Stack* s) {
+    if (Stack_isEmpty(s)) {
+        printf("Stack is empty!\n");
+        Command emptyCommand = {0, 0};
+        return emptyCommand;
+    }
+    return s->arr[(s->top)--];
+}
+
+void Stack_destroy(Stack* s) {
+    if(s->arr != NULL){
+        free(s->arr);
+        s->top = -1;
+    }
+    
 }
